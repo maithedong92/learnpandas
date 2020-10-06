035f261df6aa9645f99f1d38d0ceaaa42815ccf4
maithedong92 <72377981+maithedong92@users.noreply.github.com>
2020-10-06 11:00:29 +0700

    gaga

---
+++ newtest.py
@@ -0,0 +1,35 @@
+#new pandas things
+
+
+#import thu vien pandas
+import pandas as pd
+
+#read sample csv file
+filecsv = pd.read_csv("thongtinlienhe.csv")
+
+#hien thi 5 hang dau
+print('hien thi 5 hang dau tien')
+print('--------------------------')
+print(filecsv.head(5))
+
+#hien thi 5 hang dau
+print('hien thi 2 cot co ten STT va Email')
+print('--------------------------')
+print(filecsv[["STT","Email"]])
+
+#lay du lieu theo dieu kien STT la 5
+print('lay du lieu theo dieu kien STT la 5')
+print('--------------------------')
+print(filecsv[filecsv.STT == 5])
+
+
+#lay du lieu theo dieu kien: STT lon hon 2 nho hon 5
+print('lay du lieu theo dieu kien STT lon hon 2 nho hon 5')
+print('--------------------------')
+print(filecsv[(filecsv.STT >= 2) & (filecsv.STT <= 5)])
+
+
+#hien thi nhung email co chua chu a
+print('hien thi nhung email co chua chu a, bo qua nhung o trong')
+print('--------------------------')
+print(filecsv[filecsv.Email.str.contains('a').fillna(False)])
